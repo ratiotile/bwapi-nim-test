c21e642f308c80ffbd92e64eb44893e5e54bba00
ratiotile <eujain@gmail.com>
2017-06-06 14:07:17 -0400

    binding generator script

---
+++ generate_bindings.nim
@@ -0,0 +1,19 @@
+import os, re, strutils
+
+const bwapi_dir = "bwapi\\include"
+
+proc ensureDir(path:string)=
+  let tok = split(path, "\\")
+  let dest_dir = tok[0..^2].join(r"\")
+  if not existsOrCreateDir(dest_dir):
+    echo "created " & dest_dir
+
+for file in walkDirRec(bwapi_dir):
+  if file.match re".*\.h":
+    let dest = file
+      .replace(re"(bwapi\\include)", "bindings")
+      .replace(re"(\.h)", ".nim")
+    ensureDir(dest)
+    let cmd = "c2nim --cpp --header "&file&" -o:"&dest
+    echo cmd
+    discard execShellCmd(cmd)
